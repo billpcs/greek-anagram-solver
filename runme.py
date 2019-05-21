d583b0cbc7838088257a1b668b30ede77e199c81
billpcs <bpanagio@protonmail.com>
2019-05-21 19:16:43 +0300

    clean up

--- runme.py
+++ runme.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
 
-def haveSameLetters(dest , norm):
-	
-	dlst = list(dest)
-	nlst = list(norm)
-	dlst.sort()
-	nlst.sort()
-
-	for i in range(len(dlst)):
-		if dlst[i] != nlst[i] :
-			return False
-	return True
+def haveSameLetters(srcm, norm):
+	return (sorted(srcm) == sorted(norm))
 
 def main():
-	print("Πατήστε ENTER για να τερματίσετε το πρόγραμμα.")
+	mgs_stop = "Πατήστε ENTER για να τερματίσετε το πρόγραμμα."
+	msg_help = "Εισάγετε την ανακατεμένη λέξη. Το πρόγραμμα θα προσπαθήσει να βρεί τη λύση."
+	msg_prompt = "> "
 	words = file("dict.txt" , "r").read().split()
-	scrambled = raw_input("Εισάγετε την ανακατεμένη λέξη. Το πρόγραμμα θα προσπαθήσει να βρεί τη λύση: ")
+
+	print(mgs_stop)
+	print(msg_help)
+	scrambled = raw_input(msg_prompt)
 	while scrambled != "" :
 		answerlist = [normal for normal in words if len(normal) == len(scrambled) and haveSameLetters(scrambled,normal)]
-		for answer in answerlist : print(answer) 
-		scrambled = raw_input("Εισάγεται την ανακατεμένη λέξη. Το πρόγραμμα θα προσπαθήσει να βρεί τη λύση: ")
+		for (i, answer) in enumerate(answerlist): print("  " + str(i+1) + ". " + answer) 
+		scrambled = raw_input(msg_prompt)
 
 if __name__ == "__main__": main()
